0b10be91c896a51cd776498db5be664cc81fb5cc
Bartosz Ligęza <ligeza.bartosz@gmail.com>
2019-11-27 14:47:11 +0100

    added basic elements components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React from "react";
+import Header from "./components/elements/Header";
+import Home from "./components/Home";
 
 const App = () => {
-  return <div>react movie db</div>;
+  return (
+    <div>
+      <Header />
+      <Home />
+    </div>
+  );
 };
 
 export default App;
