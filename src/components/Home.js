481ce5d3e46afbda359a768b5789607be85362fd
Bartosz Ligęza <ligeza.bartosz@gmail.com>
2019-11-28 11:32:45 +0100

    movies grid with thumbs

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import HeroImage from 'components/elements/HeroImage';
 import SearchBar from 'components/elements/SearchBar';
 import Grid from 'components/elements/Grid';
@@ -6,23 +6,48 @@ import MovieThumb from 'components/elements/MovieThumb';
 import Spinner from 'components/elements/Spinner';
 import LoadMoreBtn from 'components/elements/LoadMoreBtn';
 import { useHomeFetch } from 'hooks/useHomeFetch';
-import { IMAGE_BASE_URL, BACKDROP_SIZE } from 'config';
+import { IMAGE_BASE_URL, BACKDROP_SIZE, POSTER_SIZE } from 'config';
+import NoImage from 'images/no_image.jpg';
 
 const Home = () => {
-  const [{ state, loading, isError }, fetchMovies] = useHomeFetch();
+  const [
+    {
+      state: { movies, currentPage, totalPages, heroImage },
+      loading,
+      isError,
+    },
+    fetchMovies,
+  ] = useHomeFetch();
+  const [searchTerm, setSearchTerm] = useState('');
 
   if (isError) return <div>Something went wrong...</div>;
-  if (!state.movies[0]) return <Spinner />;
+  if (!movies[0]) return <Spinner />;
 
   return (
     <>
       <HeroImage
-        image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.heroImage.backdrop_path}`}
-        title={state.heroImage.original_title}
-        text={state.heroImage.overview}
+        image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${heroImage.backdrop_path}`}
+        title={heroImage.original_title}
+        text={heroImage.overview}
       />
       <SearchBar />
-      <Grid />
+      <Grid header={searchTerm ? 'Search Result' : 'Popular Movies'}>
+        {movies.map(movie => {
+          return (
+            <MovieThumb
+              key={movie.id}
+              clickable
+              image={
+                movie.poster_path
+                  ? `${IMAGE_BASE_URL}${POSTER_SIZE}${movie.poster_path}`
+                  : NoImage
+              }
+              movieId={movie.id}
+              movieName={movie.original_title}
+            />
+          );
+        })}
+      </Grid>
       <MovieThumb />
       <Spinner />
       <LoadMoreBtn />
--- src/components/elements/MovieThumb.js
+++ src/components/elements/MovieThumb.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import { StyledMovieThumb } from 'components/styles/StyledMovieThumb';
 
-const MovieThumb = () => {
-  return <div>MovieThumb</div>;
-};
+const MovieThumb = ({ image, movieId, clickable }) => (
+  <StyledMovieThumb>
+    {clickable ? (
+      <img className="clickable" src={image} alt="movie_thumb" />
+    ) : (
+      <img src={image} alt="movie_thumb" />
+    )}
+  </StyledMovieThumb>
+);
 
 export default MovieThumb;
