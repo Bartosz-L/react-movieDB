1243a91c680e0818435b79bd3636fd7da256f6df
Bartosz Ligęza <ligeza.bartosz@gmail.com>
2019-11-28 13:11:57 +0100

    added routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import Header from 'components/elements/Header';
-import Home from 'components/Home';
+import Home from 'views/Home';
+import Movie from 'views/Movie';
+import NotFound from 'views/NotFound';
 import { GlobalStyle } from 'components/styles/GlobalStyle';
 
 const App = () => {
   return (
     <div>
       <Header />
-      <Home />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/:movieId" component={Movie} />
+        <Route component={NotFound} />
+      </Switch>
       <GlobalStyle />
     </div>
   );
--- src/components/elements/Header.js
+++ src/components/elements/Header.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import reactMovieLogo from 'images/reactMovie_logo.png';
 import TMDBLogo from 'images/tmdb_logo.svg';
-import { StyledHeader, StyledRMDBLogo, StyledTMDBLogo } from 'components/styles/StyledHeader';
+import {
+  StyledHeader,
+  StyledRMDBLogo,
+  StyledTMDBLogo,
+} from 'components/styles/StyledHeader';
 
 const Header = () => {
   return (
     <StyledHeader>
       <div className="header-content">
-        <StyledRMDBLogo src={reactMovieLogo} alt="reactMovie_logo" />
+        <Link to="/">
+          <StyledRMDBLogo src={reactMovieLogo} alt="reactMovie_logo" />
+        </Link>
         <StyledTMDBLogo src={TMDBLogo} alt="tmdb_logo" />
       </div>
     </StyledHeader>
