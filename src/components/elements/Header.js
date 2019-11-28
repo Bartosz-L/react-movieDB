f94ee80709b33e691fa9325a7daae97a5c8a13cf
Bartosz Ligęza <ligeza.bartosz@gmail.com>
2019-11-28 10:47:27 +0100

    created content in header component

--- src/components/elements/Header.js
+++ src/components/elements/Header.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import reactMovieLogo from 'images/reactMovie_logo.png';
+import TMDBLogo from 'images/tmdb_logo.svg';
+import { StyledHeader, StyledRMDBLogo, StyledTMDBLogo } from 'components/styles/StyledHeader';
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <StyledHeader>
+      <div className="header-content">
+        <StyledRMDBLogo src={reactMovieLogo} alt="reactMovie_logo" />
+        <StyledTMDBLogo src={TMDBLogo} alt="tmdb_logo" />
+      </div>
+    </StyledHeader>
+  );
 };
 
 export default Header;
