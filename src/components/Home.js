0eddcf0b0c493e607e25f6b05c5677452589fdb2
Bartosz Ligęza <ligeza.bartosz@gmail.com>
2019-11-28 11:01:33 +0100

    hero image component done

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,15 +6,23 @@ import MovieThumb from 'components/elements/MovieThumb';
 import Spinner from 'components/elements/Spinner';
 import LoadMoreBtn from 'components/elements/LoadMoreBtn';
 import { useHomeFetch } from 'hooks/useHomeFetch';
+import { IMAGE_BASE_URL, BACKDROP_SIZE } from 'config';
 
 const API_KEY = process.env.REACT_APP_API_KEY;
 
 const Home = () => {
   const [{ state, loading, isError }, fetchMovies] = useHomeFetch();
 
+  if (isError) return <div>Something went wrong...</div>;
+  if (!state.movies[0]) return <Spinner />;
+
   return (
     <>
-      <HeroImage />
+      <HeroImage
+        image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.heroImage.backdrop_path}`}
+        title={state.heroImage.original_title}
+        text={state.heroImage.overview}
+      />
       <SearchBar />
       <Grid />
       <MovieThumb />
